6f99615dd63b7a31317f8851924e21f5e258053a
kutlo2019 <kutlopopo@rocketmail.com>
2020-08-05 18:00:37 +0200

    styled app, need more pages

---
+++ student_portals/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class StudentPortalsConfig(AppConfig):
+    name = 'student_portals'
---
+++ student_portals/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Topic(models.Model):
+	"""A topic the user wants to note down."""
+	text = models.CharField(max_length=200) # Set the number maximum of words to 200
+	date_added = models.DateTimeField(auto_now_add=True)
+	owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+	def __str__(self):
+	 	"""Return a string representation of the model."""	
+	 	return self.text
+
+class Entry(models.Model):
+	"""Something you learned about regarding the topic."""
+	topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+	text = models.TextField() # Has no limit to the number of words
+	date_added = models.DateTimeField(auto_now_add=True)
+
+	class Meta:
+		verbose_name_plural = 'entries'
+
+	def __str__(self):
+		"""Return a string representation of the model."""
+		return f"{self.text[:50]}..."
