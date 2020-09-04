ff0130b2b81885b85919ddbfce03586989d1e72a
kutlo2019 <kutlopopo@rocketmail.com>
2020-09-04 11:53:08 +0200

    bugs-fixed

--- student_portals/models.py
+++ student_portals/models.py
@@ -1,19 +1,12 @@
 from django.db import models
-<<<<<<< HEAD
+
 from account.models import Student, Tutor
-=======
-from account.models import StudentAccount
->>>>>>> 9bc3cdd5d329c3caef0ef707d2b331a1ab034b08
 
 class Topic(models.Model):
 	"""A topic the user wants to note down."""
 	text = models.CharField(max_length=200) # Set the number maximum of words to 200
 	date_added = models.DateTimeField(auto_now_add=True)
-<<<<<<< HEAD
 	owner = models.ForeignKey(Student, on_delete=models.CASCADE)
-=======
-	owner = models.ForeignKey(StudentAccount, on_delete=models.CASCADE)
->>>>>>> 9bc3cdd5d329c3caef0ef707d2b331a1ab034b08
 
 	def __str__(self):
 	 	"""Return a string representation of the model."""	
@@ -36,11 +29,7 @@ class MathematicsAssignmentGrades(models.Model):
 	"""The weekly Assignment grades to be recorderd."""
 	math_mark = models.DecimalField(max_digits=5, decimal_places=2)
 	date_added = models.DateTimeField(auto_now_add=True)
-<<<<<<< HEAD
 	owner = models.ForeignKey(Student, on_delete=models.CASCADE)
-=======
-	#owner = models.ForeignKey(StudentAccount, on_delete=models.CASCADE)
->>>>>>> 9bc3cdd5d329c3caef0ef707d2b331a1ab034b08
 
 	def __str__(self):
 		"""Return a decimal representation of the model."""
@@ -50,11 +39,7 @@ class PhysicsAssignmentGrades(models.Model):
 	"""The weekly Assignment grades to be recorderd."""
 	physics_mark = models.DecimalField(max_digits=3, decimal_places=2)
 	date_added = models.DateTimeField(auto_now_add=True)
-<<<<<<< HEAD
 	owner = models.ForeignKey(Student, on_delete=models.CASCADE)
-=======
-	#owner = models.ForeignKey(StudentAccount, on_delete=models.CASCADE)
->>>>>>> 9bc3cdd5d329c3caef0ef707d2b331a1ab034b08
 
 	def __str__(self):
 		"""Return a decimal representation of the model."""
@@ -64,11 +49,7 @@ class ChemistryAssignmentGrades(models.Model):
 	"""The weekly Assignment grades to be recorderd."""
 	chemistry_mark = models.DecimalField(max_digits=3, decimal_places=2)
 	date_added = models.DateTimeField(auto_now_add=True)
-<<<<<<< HEAD
 	owner = models.ForeignKey(Student, on_delete=models.CASCADE)
-=======
-	#owner = models.ForeignKey(StudentAccount, on_delete=models.CASCADE)
->>>>>>> 9bc3cdd5d329c3caef0ef707d2b331a1ab034b08
 
 	def __str__(self):
 		"""Return a decimal representation of the model."""
@@ -78,11 +59,7 @@ class BiologyAssignmentGrades(models.Model):
 	"""The weekly Assignment grades to be recorderd."""
 	biology_mark = models.DecimalField(max_digits=3, decimal_places=2)
 	date_added = models.DateTimeField(auto_now_add=True)
-<<<<<<< HEAD
 	owner = models.ForeignKey(Student, on_delete=models.CASCADE)
-=======
-	#owner = models.ForeignKey(StudentAccount, on_delete=models.CASCADE)
->>>>>>> 9bc3cdd5d329c3caef0ef707d2b331a1ab034b08
 
 	def __str__(self):
 		"""Return a decimal representation of the model."""
